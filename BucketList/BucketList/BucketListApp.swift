a406f4c59c4c4c5ce166ced4223e5978bda5cc81
Marlen Mynzhassar <marlen.myn@gmail.com>
2020-12-02 17:09:17 +0600

    day 69 done

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Marlen Mynzhassar on 02.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Marlen Mynzhassar on 02.12.2020.
+//
+
+import SwiftUI
+import LocalAuthentication
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        isUnlocked = true
+                    } else {
+                        
+                    }
+                }
+            }
+        } else {
+            
+        }
+    }
+}
